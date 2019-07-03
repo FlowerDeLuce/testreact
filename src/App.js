326204373097ec092f2f7b2d0250e45f5f7f673a
daria.shapovalova <daria.shapovalova@ideus.biz>
2019-07-03 18:10:20 +0300

    nav added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 
 import React, {Component} from 'react';
 import HomeScreen from './component/screen/HomeScreen/HomeScreen';
+import { Provider, connect } from 'react-redux';
 import AppNavigator from './navigation/navConfig';
+import { addNavigationHelpers } from 'react-navigation'
 console.disableYellowBox = true;
 
 export let navigatorRef;
-export default class App extends Component {
+class App extends Component {
 
   componentDidUpdate() {
     navigatorRef = this.navigator;
@@ -19,9 +21,22 @@ export default class App extends Component {
   }
 
   render() {
-    return (
-        <HomeScreen />
-    );
+      const { dispatch, nav } = this.props
+      const navigation = addNavigationHelpers({
+          dispatch,
+          state: nav
+      })
+      return <AppNavigator navigation={navigation} />
   }
 }
 
+const mapStateToProps = ({ nav }) => ({ nav })
+
+const RootNavigationStack = connect(mapStateToProps)(App);
+
+const Root = () => (
+    <Provider >
+        <RootNavigationStack />
+    </Provider>
+)
+export default Root
